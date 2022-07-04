30a887efcec12823dc8ae6b70a39977b1712a50f
Nikolai Makarevich <nikoladze529@mail.ru>
2022-07-05 00:20:26 +0300

    feat: add HttpModule, userService userResolver, dotenv

--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -5,23 +5,30 @@ import { ArtistsService } from './artists/services/artists.service';
 import { BandsService } from './bands/services/bands.service';
 import { GenresService } from './genres/services/genres.service';
 import { TracksService } from './tracks/services/tracks.service';
+import { UsersService } from './users/services/users.service';
 import { ArtistsResolver } from './artists/resolvers/artists.resolver';
 import { BandsResolver } from './bands/resolvers/bands.resolver';
 import { GenresResolver } from './genres/resolvers/genres.resolver';
 import { TracksResolver } from './tracks/resolvers/tracks.resolver';
+import { UsersResolver } from './users/resolvers/users.resolver';
+import { HttpModule } from '@nestjs/axios';
+import 'dotenv/config';
 
 @Module({
+  imports: [HttpModule],
   providers: [
     AlbumsService,
     ArtistsService,
     BandsService,
     GenresService,
     TracksService,
+    UsersService,
     AlbumsResolver,
     ArtistsResolver,
     BandsResolver,
     GenresResolver,
     TracksResolver,
+    UsersResolver,
   ],
 })
 export class ModulesModule {}
